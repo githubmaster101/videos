6d8bef7c911b868a434aaa305fc7a0488a09c7bb
Kevin <yardman10550@gmail.com>
2019-11-10 12:38:26 -0500

    setup event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    handleChange = (e) => {
+        this.setState({ term: e.target.value });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // Todo: setup callback from parent component
+    };
+
     render() {
-        return (<div>Search Bar</div>)
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.handleSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
     }
 }
 
