fa3df2402a4c03d3571a352e3d225db0294c8c28
Kevin <yardman10550@gmail.com>
2019-11-24 15:02:22 -0500

    Communicating child to parent using callbacks

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,12 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
-    return (<div>
-        <img src={video.snippet.thumbnails.medium.url} />
-        {video.snippet.title}
+const VideoItem = ({ video, onVideoSelect }) => {
+    return (<div onClick={() => onVideoSelect(video)} className="video-item item" >
+        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+        <div className="content">
+            <div className="header" >{video.snippet.title}</div>
+        </div>
     </div>);
 };
 
